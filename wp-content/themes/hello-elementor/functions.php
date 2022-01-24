e6a33831124a3974274483bb94bcf5f7a8121147
Morrinok <65527532+Morrinok@users.noreply.github.com>
2022-01-23 22:07:48 -0300

    Actualizaciones Realizadas
    Pagina web actualizada.

--- wp-content/themes/hello-elementor/functions.php
+++ wp-content/themes/hello-elementor/functions.php
@@ -216,3 +216,5 @@ if ( ! function_exists( 'hello_elementor_body_open' ) ) {
 		}
 	}
 }
+
+add_filter( 'wp_image_editors', 'change_graphic_lib' ); function change_graphic_lib($array) { return array( 'WP_Image_Editor_GD', 'WP_Image_Editor_Imagick' ); }
